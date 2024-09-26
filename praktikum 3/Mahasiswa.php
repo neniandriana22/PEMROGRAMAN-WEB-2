49ecf5efa06c0c62f11286ff012951ec3393839d
neniandriana22 <nenijambi05@gmail.com>
2024-09-25 23:06:48 -1200

    TUGAS 3

---
+++ praktikum 3/Mahasiswa.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "Orang.php";
+require_once "Nilai.php";
+
+class Mahasiswa extends Orang{
+    protected string $nim;
+    protected string $prodi;
+    protected Nilai $nilai;
+
+    public function setNim($nim){
+        $this->nim = $nim;
+    }
+    public function setProdi($prodi){
+        $this->prodi = $prodi;
+    }
+    public function setNilai ($nilai){
+        $this->nilai = $nilai;
+    }
+    public function tampilkanData(){
+        echo "Nama : " . $this->nama."<br>";
+        echo "NIM : " . $this->nim. "<br>";
+        echo "Prodi : " . $this->prodi. "<br>";
+        echo "Nilai Tugas : " . $this->nilai->getTugas() . "<br>";
+        echo "Nilai UTS : " . $this->nilai->getUts() . "<br>";
+        echo "Nilai UAS : " . $this->nilai->getUas() . "<br>";
+    }
+}
