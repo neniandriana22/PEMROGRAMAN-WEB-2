7ea975e52c9ee8cbdeb130c80dfecd48238ad9db
neniandriana22 <nenijambi05@gmail.com>
2024-11-30 08:25:20 +0700

    TUGAS 10

---
+++ toko-buku-online/app/Views/submit.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout');?>
+
+<?= $this->section('main') ?>
+
+<div class="container p-5">
+    <div class="alert alert-success">
+        <strong>Success</strong> Order Anda Sedang Di Proses, Silakan Konfirmasi Pembayaran Anda ke Whatsaap Admin
+        <a href="https://wa.me/qr/SOPTAT7HAIWQN1" class="btn btn-success">Hubungi</a>
+    </div>
+</div>
+<?= $this->endSection()?>   
